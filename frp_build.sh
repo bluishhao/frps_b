d84503fe8b766972e532d51a37a107909d05de3c
bluishhao <85738699+bluishhao@users.noreply.github.com>
2022-09-21 18:27:07 +0800

    Add files via upload

--- frp_build.sh
+++ frp_build.sh
@@ -3,5 +3,5 @@ tar -zxvf frp_0.44.0_linux_amd64.tar.gz
 cd frp_0.44.0_linux_amd64
 cp frps.ini frps.ini.bak
 rm frps.ini
-wget https://raw.githubusercontent.com/bluishhao/-/main/frps.ini?token=GHSAT0AAAAAABZAWNG44JGUAQYHKMRAK4RYYZK4N6Q
+cp ../frps.ini ./frps.ini
 ./frps -c frps.ini
