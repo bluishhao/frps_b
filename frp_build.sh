10785e72f67594ba4c17a741447fe11b80878d54
bluishhao <85738699+bluishhao@users.noreply.github.com>
2022-09-21 18:03:16 +0800

    Update frp_build.sh

--- frp_build.sh
+++ frp_build.sh
@@ -3,5 +3,5 @@ tar -zxvf frp_0.44.0_linux_amd64.tar.gz
 cd frp_0.44.0_linux_amd64
 cp frps.ini frps.ini.bak
 rm frps.ini
-wget https://github.com/bluishhao/-/blob/main/frps.ini
+wget https://raw.githubusercontent.com/bluishhao/-/main/frps.ini?token=GHSAT0AAAAAABZAWNG4IP5ZUKFM64E22OKIYZK4GEA
 ./frps -c frps.ini
