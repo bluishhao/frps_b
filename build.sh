e4ebfeec9144c62786179722d4160411fea42aa0
bluishhao <85738699+bluishhao@users.noreply.github.com>
2022-09-22 23:31:22 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ wget https://github.com/fatedier/frp/releases/download/v0.44.0/frp_0.44.0_linux_
 sudo git clone https://github.com/bluishhao/myddns.git
 tar -zxvf frp_0.44.0_linux_amd64.tar.gz
 cp ./frps_b/frps.ini ./frp_0.44.0_linux_amd64/frps.ini
-cd myddns
-tar -zxvf cloudflare-ddns-client.tar
+cd myddns/
+tar -xzvf cloudflare-ddns-client.tar
 cd cloudflare-ddns-client
 sudo make install
 cloudflare-ddns --configure
