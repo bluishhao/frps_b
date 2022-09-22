8eea72abe1375b65d4d3845439acb50192de88b8
bluishhao <85738699+bluishhao@users.noreply.github.com>
2022-09-22 23:05:36 +0800

    Delete frp_build.sh

--- frp_build.sh
+++
@@ -1,7 +0,0 @@
-wget https://github.com/fatedier/frp/releases/download/v0.44.0/frp_0.44.0_linux_amd64.tar.gz
-tar -zxvf frp_0.44.0_linux_amd64.tar.gz
-cd frp_0.44.0_linux_amd64
-cp frps.ini frps.ini.bak
-rm frps.ini
-cp ../frps.ini ./frps.ini
-./frps -c frps.ini
